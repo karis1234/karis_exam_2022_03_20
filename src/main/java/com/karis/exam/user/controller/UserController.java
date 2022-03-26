46db593478e6d31f5d9ed480f247c54a352c81ac
karis1234 <qkzkf3487@naver.com>
2022-03-26 15:52:20 +0900

    User 객체 만들어서 리턴

--- src/main/java/com/karis/exam/user/controller/UserController.java
+++ src/main/java/com/karis/exam/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.karis.exam.user.controller;
 
+import com.karis.exam.user.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,14 +15,14 @@ public class UserController {
     @RequestMapping("")
     @ResponseBody
     public String users() {
-        return "유저 여러명!!!!!";
+        return "유저 여러명!!!";
     }
 
     @RequestMapping("1")
     @ResponseBody
-    public String user() {
-
-        return "유저1명";
+    public User user() {
+        User user1 = new User();
+        return user1;
     }
 
 
--- src/main/java/com/karis/exam/user/domain/User.java
+++ src/main/java/com/karis/exam/user/domain/User.java
@@ -1,9 +1,7 @@
 package com.karis.exam.user.domain;
 
-import javax.persistence.Entity;
 import java.time.LocalDateTime;
 
-@Entity
 public class User {
     public long id;
     public LocalDateTime regDate;
