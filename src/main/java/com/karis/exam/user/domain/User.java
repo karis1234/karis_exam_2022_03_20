b4eca0a9e5b077ab2dc8ae8dcfd89f57d9376a65
karis1234 <qkzkf3487@naver.com>
2022-03-26 15:43:26 +0900

    User 클래스 수정

--- src/main/java/com/karis/exam/user/domain/User.java
+++ src/main/java/com/karis/exam/user/domain/User.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 public class User {
     public long id;
     public LocalDateTime regDate;
-    public java.time.LocalDateTime updateDate;
+    public LocalDateTime updateDate;
     public String email;
     public String password;
     public String name;
